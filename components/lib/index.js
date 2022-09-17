d9fee95b47183dd08647277da6cae79130289b56
周爱桩 <840043348@qq.com>
2022-09-17 14:33:32 +0800

    完善按需引入

--- components/lib/index.js
+++ components/lib/index.js
@@ -1,20 +1,18 @@
 import Demo from "./demo/index.js"
 import Card from "./card/index.js"
+export { Demo, Card }
 
 const components = {
   Demo,
   Card
 }
-
 const install = function (Vue) {
   if(install.installed) return ;
   Object.keys(components).forEach(key => {
     Vue.component(components[key].name, components[key])
   })
 }
-
 const API = {
   install
 }
-
 export default API
--- examples/main.js
+++ examples/main.js
@@ -1,12 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import "../components/css/demo.scss"
-import "../components/css/card.scss"
-import Demo from '../components/lib/demo/index.js'
-import Card from '../components/lib/card/index.js'
-Vue.use(Demo)
-Vue.use(Card)
+/*
+ ******************************************************************************************
+ *****************************************本地调试方式**************************************
+ ******************************************************************************************
+*/
+// 1、全局引入
+// import "../components/css/index.css"
+// import GAGCUI from '../components/lib/index.js'
+// Vue.use(GAGCUI)
+// 2、按需引入
+// import "../components/css/demo.scss"
+// import "../components/css/card.scss"
+// import { Demo, Card } from "../components/lib/index.js"
+// Vue.use(Demo)
+// Vue.use(Card)
+/************************************************************************************** */
+
+/*
+ *****************************************************************************************
+ *****************************************其他项目，通过引入线上代码看效果****************************
+ *****************************************************************************************
+*/
+// 1、全局引入
+// import 'gagc-ui/dist/css/index.css'
+// import GAGCUI from 'gagc-ui'
+// Vue.use(GAGCUI)
+// 2、按需引入
+// import 'gagc-ui/dist/css/demo.css'
+// import 'gagc-ui/dist/css/card.css'
+// import { Demo, Card } from "gagc-ui"
+// Vue.use(Demo)
+// Vue.use(Card)
 
 Vue.config.productionTip = false
 
